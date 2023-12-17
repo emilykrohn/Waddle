d3bb394fda5e87f523980b8055a58474df7a2c23
Emily Krohn <emilykrohn2@gmail.com>
2023-12-17 10:52:21 -0800

    Play walking animation

--- Scripts/PlayerMovement.gd
+++ Scripts/PlayerMovement.gd
@@ -1,6 +1,11 @@
 extends Node
 
+var direction
+
 func movement(speed):
 	var direction = Input.get_vector("left", "right", "up", "down")
 	get_parent().velocity = direction * speed
-	
+	if direction != Vector2.ZERO:
+		$%AnimationPlayer.play()
+	else:
+		$%AnimationPlayer.stop()
