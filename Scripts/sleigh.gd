a2e16c52f22b265b466f20b3989db696aec0a1b1
Emily Krohn <emilykrohn2@gmail.com>
2023-12-21 08:32:08 -0800

    Increase presents needed to be collected

--- Scripts/sleigh.gd
+++ Scripts/sleigh.gd
@@ -1,11 +1,11 @@
 extends Sprite2D
 
 func _process(_delta):
-	if $%Player/Collect.presents_collected == 2:
-		region_rect = Rect2(32,0,32,32)
 	if $%Player/Collect.presents_collected == 4:
-		region_rect = Rect2(64,0,32,32)
-	if $%Player/Collect.presents_collected == 6:
-		region_rect = Rect2(96,0,32,32)
+		region_rect = Rect2(32,0,32,32)
 	if $%Player/Collect.presents_collected == 8:
+		region_rect = Rect2(64,0,32,32)
+	if $%Player/Collect.presents_collected == 12:
+		region_rect = Rect2(96,0,32,32)
+	if $%Player/Collect.presents_collected == 16:
 		region_rect = Rect2(128,0,32,32)
