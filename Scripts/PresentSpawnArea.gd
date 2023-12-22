55f5b4ab8a56744f45180796388b95212a825969
Emily Krohn <emilykrohn2@gmail.com>
2023-12-22 08:25:40 -0800

    Put title screen in same scene as world

--- Scripts/PresentSpawnArea.gd
+++ Scripts/PresentSpawnArea.gd
@@ -3,7 +3,7 @@ extends Path2D
 var rng = RandomNumberGenerator.new()
 
 func _on_timer_timeout():
-	if not $%Player.player_won:
+	if not $%Player.player_won and $%Player.game_start:
 		var scene = preload("res://Scenes/path_follow_2d.tscn").instantiate()
 		add_child(scene)
 		scene.progress_ratio = rng.randf()
