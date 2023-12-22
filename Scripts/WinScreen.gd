7fc2f1d54644147998776911c9e54094486ed71e
Emily Krohn <emilykrohn2@gmail.com>
2023-12-22 08:48:55 -0800

    Add button clicking sounds to title and win scene

--- Scripts/WinScreen.gd
+++ Scripts/WinScreen.gd
@@ -1,10 +1,17 @@
 extends Control
 
+var can_restart := false
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if $%Player.player_won:
 		visible = true
-
+	if can_restart:
+		get_tree().reload_current_scene()
 
 func _on_button_pressed():
-	get_tree().reload_current_scene()
+	$%ButtonSound.play()
+	$Timer.start()
+
+func _on_timer_timeout():
+	can_restart = true
