9bad38938b86f42a361121c32d9e743a722eb15b
Emily Krohn <emilykrohn2@gmail.com>
2023-12-22 09:10:16 -0800

    Player walking sounds

--- Scripts/PlayerMovement.gd
+++ Scripts/PlayerMovement.gd
@@ -1,11 +1,16 @@
 extends Node
 
 var direction
+var is_walking := false
+
+
 
 func movement(speed):
 	direction = Input.get_vector("left", "right", "up", "down")
 	get_parent().velocity = direction * speed
 	if direction != Vector2.ZERO:
 		$%AnimationPlayer.play()
+		is_walking = true
 	else:
 		$%AnimationPlayer.stop()
+		is_walking = false
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -7,9 +7,16 @@ var game_start := false
 func _physics_process(_delta):
 	if not player_won and game_start:
 		$%PlayerMovement.movement(SPEED)
+		if $%PlayerMovement.is_walking:
+			$"../../WalkSound".play()
 	
 	move_and_slide()
 
 
 func _on_button_pressed():
 	game_start = true
+
+
+func _on_walk_sound_finished():
+	if $%PlayerMovement.is_walking:
+		$"../../WalkSound".play()
